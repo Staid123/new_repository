f3a27464514cd6cb5ebf3264be2ee459253a4615
Kirill Pavlenko <pavlenkokirill120@gmail.com>
2023-07-14 14:39:51 +0300

    Обновленная версия Эхо бота через регистрацию в диспечере

--- pythonFILE.py
+++ pythonFILE.py
@@ -1,4 +1,4 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
 
@@ -11,22 +11,59 @@ dp: Dispatcher = Dispatcher()
 
 
 # Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands=["start"]))
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
 
 # Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=['help']))
 async def process_help_command(message: Message):
     await message.answer('Напиши мне что-нибудь и в ответ '
                          'я пришлю тебе твое сообщение')
 
+
+# Этот хэндлер будет срабатывать на любые ваши фото
+# кроме команд "/start" и "/help"
+async def send_photo_echo(message: Message):
+    await message.answer_photo(message.photo[0].file_id)
+
+
+# Этот хэндлер будет срабатывать на любые ваши голосовые сообщения
+# кроме команд "/start" и "/help"
+async def send_voice_echo(message: Message):
+    print(message)
+    await message.answer_voice(message.voice.file_id)
+
+
+# Этот хэндлер будет срабатывать на любые ваши стикеры
+# кроме команд "/start" и "/help"
+async def send_sticker_echo(message: Message):
+    await message.answer_sticker(message.sticker.file_id)
+
+
+# Этот хэндлер будет срабатывать на любые ваши видео
+# кроме команд "/start" и "/help"
+async def send_video_echo(message: Message):
+    await message.answer_video(message.video.file_id)
+
+async def send_video_note_echo(message: Message):
+    await message.answer_video_note(message.video_note.file_id)
+
+
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
-@dp.message()
 async def send_echo(message: Message):
     await message.reply(text=message.text)
+
+
+dp.message.register(process_start_command, Command(commands=['start']))
+dp.message.register(process_help_command, Command(commands=['help']))
+dp.message.register(send_photo_echo, F.photo)
+dp.message.register(send_voice_echo, F.voice)
+dp.message.register(send_sticker_echo, F.sticker)
+dp.message.register(send_video_echo, F.video)
+dp.message.register(send_video_note_echo, F.video_note)
+dp.message.register(send_echo)
+
 
 if __name__ == '__main__':
     dp.run_polling(bot)
