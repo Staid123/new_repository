fcaca2a293d73e689325e4c441cfab2c7decc732
Kirill Pavlenko <pavlenkokirill120@gmail.com>
2023-07-14 15:36:37 +0300

    Обновленная версия телеграмм бота через регистарцию в диспечере

--- pythonFILE.py
+++ pythonFILE.py
@@ -45,13 +45,28 @@ async def send_sticker_echo(message: Message):
 async def send_video_echo(message: Message):
     await message.answer_video(message.video.file_id)
 
-async def send_video_note_echo(message: Message):
-    await message.answer_video_note(message.video_note.file_id)
+
+# Этот хэндлер будет срабатывать на любые ваши аудио сообщения
+# кроме команд "/start" и "/help"
+async def send_audio_echo(message: Message):
+    await message.audio(message.audio.file_id)
+
+
+# Этот хэндлер будет срабатывать на любые ваши анимации
+# кроме команд "/start" и "/help"
+async def send_animation_echo(message: Message):
+    await message.animation(message.animation.file_id)
+
+# Этот хэндлер будет срабатывать на любые ваши документы
+# кроме команд "/start" и "/help"
+async def send_document_echo(message: Message):
+    await message.document(message.document.file_id)
 
 
 # Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
 # кроме команд "/start" и "/help"
 async def send_echo(message: Message):
+    print(message.chat.first_name)
     await message.reply(text=message.text)
 
 
@@ -61,7 +76,9 @@ dp.message.register(send_photo_echo, F.photo)
 dp.message.register(send_voice_echo, F.voice)
 dp.message.register(send_sticker_echo, F.sticker)
 dp.message.register(send_video_echo, F.video)
-dp.message.register(send_video_note_echo, F.video_note)
+dp.message.register(send_audio_echo, F.audio)
+dp.message.register(send_animation_echo, F.animation)
+dp.message.register(send_document_echo, F.document)
 dp.message.register(send_echo)
 
 
